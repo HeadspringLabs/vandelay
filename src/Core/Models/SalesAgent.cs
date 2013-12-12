bf3442c631903ee420037b4d1b14e2e584454324
Chris Missal <chris.missal@gmail.com>
2013-12-12 17:23:56 -0600

    add more properties to SalesAgent

--- src/Core/Models/SalesAgent.cs
+++ src/Core/Models/SalesAgent.cs
@@ -1,7 +1,15 @@
 namespace Core.Models
 {
+    using System.Collections.Generic;
+
     public class SalesAgent : Entity
     {
+        public string ImageUrl { get; set; }
+
         public Location Location { get; set; }
+
+        public List<Export> Exports { get; set; }
+
+        public List<Import> Imports { get; set; }
     }
 }
