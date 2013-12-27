9070bf4ebff4d1fd05891abd27539e6085568526
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2013-12-27 15:57:47 -0600

    Added Ember-Model for JSON getting, simple location route/view

--- src/Core/Scripts/app.js
+++ src/Core/Scripts/app.js
@@ -1,1 +1,18 @@
 ﻿App = Ember.Application.create();
+
+App.Adapter = Ember.RESTAdapter.extend({
+	buildURL: function (record, suffix) {
+		var s = this._super(record, suffix);
+		return s.substring(0, s.length - 5);
+	},
+	ajaxSettings: function (url, method) {
+		return {
+			url: url,
+			type: method
+		};
+	}
+});
+
+App.Router.map(function () {
+	this.route("locations");
+});
