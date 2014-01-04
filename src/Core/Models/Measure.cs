a2ee8719c38f9e4f0f63f0d9219c2610e7f611b4
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2014-01-04 15:50:57 -0600

    Fix duplicate enum values in Measure

--- src/Core/Models/Measure.cs
+++ src/Core/Models/Measure.cs
@@ -8,13 +8,13 @@ namespace Core.Models
         public static readonly Measure CorrugatedBox = new Measure(2, "Corrugated box");
         public static readonly Measure WoodenBox = new Measure(3, "Wooden box");
         public static readonly Measure Crate = new Measure(4, "Crate");
-        public static readonly Measure BulkContainer = new Measure(4, "Intermediate bulk shipping container");
-        public static readonly Measure FlexibleContainer = new Measure(4, "Flexible Intermediate bulk container");
         public static readonly Measure BulkBox = new Measure(5, "Bulk box");
         public static readonly Measure Drum = new Measure(6, "Drum");
         public static readonly Measure InsulatedContainer = new Measure(7, "Insulated shipping container");
 
         public static readonly Measure Each = new Measure(8, "Each");
+		public static readonly Measure BulkContainer = new Measure(9, "Intermediate bulk shipping container");
+		public static readonly Measure FlexibleContainer = new Measure(10, "Flexible Intermediate bulk container");
 
         public Measure(int value, string displayName) : base(value, displayName)
         {
