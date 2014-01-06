cbbb6b0e0b93f3f32bfac176dbcd44aec9b106e7
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2014-01-06 01:56:03 -0600

    Add computed net import & export values

--- src/Core/Scripts/Pages/salesAgents.js
+++ src/Core/Scripts/Pages/salesAgents.js
@@ -13,3 +13,22 @@ App.SalesAgentsDetailRoute = Ember.Route.extend({
 		return App.SalesAgent.findById(params.id);
 	}
 });
+
+App.SalesAgentsDetailController = Ember.ObjectController.extend({
+	netImports: function () {
+		var sum = 0;
+		var imports = this.get('imports');
+		$(imports).each(function(index, imp) {
+			sum += (imp.price * imp.quantity);
+		});
+		return sum;
+	}.property('model.imports'),
+	netExports: function(){
+		var sum = 0;
+		var exports = this.get('exports');
+		$(exports).each(function (index, exp) {
+			sum += (exp.price * exp.quantity);
+		});
+		return sum;
+	}.property('model.exports')
+});
