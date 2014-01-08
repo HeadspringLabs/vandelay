332ec164025db71e7439ce8a6965fbb0c65556b1
Chris Missal <chris.missal@gmail.com>
2014-01-08 01:09:18 -0600

    add create and delete for agents and locations

--- src/Core/Content/app/js/controllers.js
+++ src/Core/Content/app/js/controllers.js
@@ -4,9 +4,37 @@
 
 vandelayApp.controller('SalesAgentCtrl', ['$scope', 'SalesAgent', function ($scope, SalesAgent) {
     $scope.agents = SalesAgent.query();
+
+    $scope.save = function () {
+        SalesAgent.save($scope.agent, function (agent) {
+            $scope.agents.push(agent);
+            $scope.agent = {};
+        });
+    };
+
+    $scope.delete = function (agent) {
+        SalesAgent.delete(agent, function () {
+            var index = $scope.agents.indexOf(agent);
+            $scope.agents.splice(index, 1);
+        });
+    };
 }]);
 
 vandelayApp.controller('LocationCtrl', ['$scope', 'Location', function ($scope, Location) {
     $scope.locations = Location.query();
+
+    $scope.save = function () {
+        Location.save($scope.location, function (location) {
+            $scope.locations.push(location);
+            $scope.location = {};
+        });
+    };
+
+    $scope.delete = function (location) {
+        Location.delete(location, function () {
+            var index = $scope.locations.indexOf(location);
+            $scope.locations.splice(index, 1);
+        });
+    };
 }]);
 
