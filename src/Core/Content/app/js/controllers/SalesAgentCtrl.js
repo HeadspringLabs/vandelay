f743c9d5e65be854e1e3307991ff9a98f324445b
Chris Missal <chris.missal@gmail.com>
2014-01-10 00:57:53 -0600

    add ability to add/remove imports for sales agents

--- src/Core/Content/app/js/controllers/SalesAgentCtrl.js
+++ src/Core/Content/app/js/controllers/SalesAgentCtrl.js
@@ -1,5 +1,7 @@
-﻿vandelayApp.controller('SalesAgentCtrl', ['$scope', '$routeParams', 'SalesAgent', function ($scope, $routeParams, SalesAgent) {
+﻿vandelayApp.controller('SalesAgentCtrl', ['$scope', '$routeParams', 'SalesAgent', 'Import', function ($scope, $routeParams, SalesAgent, Import) {
     $scope.agents = SalesAgent.query();
+    $scope.availableImports = Import.query();
+
     $routeParams.id && ($scope.agent = SalesAgent.get($routeParams));
 
     $scope.save = function () {
@@ -14,5 +16,20 @@
             var index = $scope.agents.indexOf(agent);
             $scope.agents.splice(index, 1);
         });
+    };
+
+    $scope.add = function (_import) {
+        $scope.agent.imports = $scope.agent.imports || [];
+        $scope.agent.imports.push(_import);
+        SalesAgent.save($scope.agent, function () {
+            var index = $scope.availableImports.indexOf(_import);
+            $scope.availableImports.splice(index, 1);
+        });
+    };
+    
+    $scope.remove = function (_import) {
+        var index = $scope.agent.imports.indexOf(_import);
+        $scope.agent.imports.splice(index, 1);
+        SalesAgent.save($scope.agent);
     };
 }]);
